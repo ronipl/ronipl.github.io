c425a629d00e66154b3cbc758904e8bf261bcf6c
roni <ronipinili@yahoo.com>
2016-06-30 21:12:42 +0800

    pangalawang gumawa

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["My Name is Roni", "I'm a Web Frontend Web Developer"],   
         typeSpeed: 100,
         backDelay: 900,
         // loop
